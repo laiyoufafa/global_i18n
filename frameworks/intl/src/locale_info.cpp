bdd3a50ba2d77bbc17567a41ca9d3681f54e1542
sunyaozu <sunyaozu@huawei.com>
2022-06-06 21:28:34 +0800

    fix locale_info constructor invalid parameter result in crash
    Signed-off-by: sunyaozu <sunyaozu@huawei.com>

--- frameworks/intl/src/locale_info.cpp
+++ frameworks/intl/src/locale_info.cpp
@@ -80,6 +80,8 @@ LocaleInfo::LocaleInfo(const std::string &localeTag, std::map<std::string, std::
         std::string defaultLocaleTag = LocaleConfig::GetSystemLocale();
         finalLocaleTag = "";
         ComputeFinalLocaleTag(defaultLocaleTag);
+        status = U_ZERO_ERROR;
+        builder->clear();
         locale = builder->setLanguageTag(StringPiece(finalLocaleTag)).build(status);
     }
     language = locale.getLanguage();
